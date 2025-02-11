22640748d1fa7c00648fe04a85873fa973e8f817
Carlos Reyes <andrespk3000@gmail.com>
2025-02-10 22:27:59 -0500

    user creation form --missing, navbar --c, and home view --m

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+
+def signup_view(request):
+
+    context = {
+        'form': UserCreationForm
+    }
+
+    return render(request, 'users/signup.html', context)
