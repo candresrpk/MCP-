485f945b974f5bdc69678a19d41b2b53505d7e3d
carlos reyes <andrespk3000@gmail.com>
2025-02-11 17:45:50 -0500

    comando para activar entorno e instalar dependencias

---
+++ environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+	
+
+s-venv 
+
+echo "environment activated"
+echo "installing dependences"
+sleep 3s
+
+pip install -r requirements.txt
+
+
+echo "dependences where installed successfully"
